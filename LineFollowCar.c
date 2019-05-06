22b54ecb12734b00e66a348d397c50334a1b38c8
Neuroplant <m.roych@gmail.com>
2019-05-06 21:12:47 +0200

    Update LineFollowCar.c

--- LineFollowCar.c
+++ LineFollowCar.c
@@ -214,171 +214,6 @@ void init_Encoder(void) {
 }
 
 
-//	JoyStick  ///////////////////////////////////////////////////////////////////////
-const char *device = "/dev/input/js0";
-int js;
-struct js_event event;
-size_t axis;
-
-int read_event(int JoyHandle, struct js_event *event) {
-    ssize_t bytes;
-    bytes = read(JoyHandle, event, sizeof(*event));
-    if (bytes == sizeof(*event)) return 0;
-    /* Error, could not read full event. */
-    return -1;
-}
-
-size_t get_axis_count(int JoyHandle) {
-    __u8 axes;
-    if (ioctl(JoyHandle, JSIOCGAXES, &axes) == -1)
-        return 0;
-    return axes;
-}
-
-size_t get_button_count(int JoyHandle) {
-    __u8 buttons;
-    if (ioctl(JoyHandle, JSIOCGBUTTONS, &buttons) == -1)
-        return 0;
-    return buttons;
-}
-
-
-int StickControl(int stick, int value) {
-	switch (stick) {
-		case 0 :	//L3 Left/Right
-		break;
-		case 1 :	//L3 Up/Down
-				
-		break;
-		case 2 :	//L2 Pull
-		//	speed=(map(value,-32767,32767,-SPEED_MAX,0));		//Rückwärts
-		break;
-		case 3 :	//R3 Left/Right
-				steering=(map(value,32767,-32767,10,-10));
-					//Blinker
-				if (steering == 10 ) {
-					Blinker[1].dura = 2;
-					Blinker[1].freq = 2;
-					Blinker[3].dura = 2;
-					Blinker[3].freq = 2;
-				}
-				if (steering == -10 ) {
-					Blinker[2].dura = 2;
-					Blinker[2].freq = 2;
-					Blinker[4].dura = 2;
-					Blinker[4].freq = 2;					
-				}
-		break;
-		case 4 :	//R3 Up/Down
-		break;
-		case 5 :	//R2 Pull
-				    Spin_Target = (map(value,-32767,32767,0,(int)SPIN_MAX));
-		break;
-	}
-	return 0;
-}
-
-int ButtonControl (int button, int value) {
-//  PRESS							RELEASE
-/*
-	X		Hupe					-
-	O    	Scheinwerfer (60s)		-
-	Dreieck	Laser Beam on			Laser Beam off			
-	Quadrat	encoder_mode on/off
-	Up	move Turret1 up			Turret1 Stop
-	Down	move Turret1 down		Turret1 Stop
-	Left	move Turret1 left		Turret1 Stop
-	Right	move Turret1 right		Turret1 Stop
-	L1	Rückwärts			Vorwärts
-	L2		
-	L3		-
-	R1		Brake                   Brake off
-	R2		-
-	R3		-
-	Select	-
-	Option	-
-	PS		Quit
-*/
-	if (event.value==1) {
-		switch (button) {
-			case 0 :		//	X
-				Sound[1].loop = 2;
-			break;
-			case 1	:		//	O
-					//Scheinwerfer
-				Blinker[5].dura = 60;
-				Blinker[5].freq = 0;
-			break;
-			case 2 :		//Dreieck
-				//turret1 = 3;
-			break;
-			case 3 :		//quadrat
-			break;
-			case 4	:	//L1
-				gear = -1;
-			break;
-					
-			//Turret1 Control
-			case 13 :	//UP
-			//   	turret1 = 8;
-			break;
-			case 14 :	//DOWN
-    			//	turret1 = 2;
-			break;
-			case 15 :	//LEFT
-			 //   	turret1 = 4;
-			break;
-			case 16 :	//RIGHT
-    			//	turret1 = 6;
-			break;
-			case 5 :    //R1
-                    		gear = 0;
-			break;
-			case 6 :	//L2
-			break;
-			case 10 :	//PS
-				run = false;
-			break;
-			default :	//noch ohne Funktion
-			printf("Button Nr.%i pressed\n", button);
-			}
-	} else {
-		switch (button) {
-			case 0 :		//	X
-			break;
-			case 1	:		//	O
-			break;
-			case 2 :		//Dreieck
-    			//turret1 = 9;
-			break;
-			case 4	:		//L1
-				gear = 1;
-			break;
-			//Turret1 Control
-			case 13 :		//UP
-			//	turret1 =  0;
-			break;
-			case 14 :		//DOWN
-			//	turret1 =  0;
-			break;
-			case 15 :		//LEFT
-			//	turret1 =  0;
-			break;
-			case 16 :		//RIGHT
-			//	turret1 =  0;
-			break;
-			case 5 :    		//R1
-				gear = 1;
-			break;
-			case 10 :	//PS
-			break;
-			default :	//noch ohne Funktion
-			printf("Button Nr.%i released\n", button);
-		}
-		
-	}
-	return 0;
-}
 float getSonar();
 
 void *MCPThread (void *value) {
@@ -473,11 +308,12 @@ int main (int argc, char *argv[]) {/////////////////////////////////////////////
         printf("setup wiringPi faiservo !");
         return 1; 
 	};
+//I2C
 	if(wiringPiI2CSetup(DEV_ID0) == -1){ 
        	printf("setup wiringPi I2C faiservo !");
        	return 1; 
 	};
-	
+//ServoDriver
 	int SDHandle = pca9685Setup(PIN_BASE0, DEV_ID0, HERTZ);
 	if (SDHandle < 0) {
 		printf("Error in setup\n");
@@ -486,6 +322,7 @@ int main (int argc, char *argv[]) {/////////////////////////////////////////////
 	pinMode(DEV_ID0_enable,OUTPUT);
 	digitalWrite(DEV_ID0_enable,LOW);
 	
+//LCD
 	pcf8574Setup(PIN_BASE1,DEV_ID1);// initialize PCF8574 
 	for(i=0;i<8;i++){ 
 		pinMode(PIN_BASE1+i,OUTPUT); // set PCF8574 port to output mode 
@@ -505,8 +342,8 @@ int main (int argc, char *argv[]) {/////////////////////////////////////////////
 //Blinker
 	init_Blinker();
 //Turret
-	servoInit(servoPin_CX);	// X
-	servoInit(servoPin_CY);	// Y
+	//servoInit(servoPin_CX);	// X
+	//servoInit(servoPin_CY);	// Y
 	//pthread_t t_Turret;
         //if(pthread_create(&t_Turret, NULL, TurretThread, NULL)) {
 	//		printf("Error creating thread t_Turret\n");
@@ -519,7 +356,7 @@ int main (int argc, char *argv[]) {/////////////////////////////////////////////
 		return 1;
 	}	
 //Joystick init
-	js = open(device, O_RDONLY);
+	//js = open(device, O_RDONLY);
 	/*while (js == -1) {
 	    	js = open(device, O_RDONLY);
         	Blinker[5].dura = 2;
@@ -539,6 +376,10 @@ int main (int argc, char *argv[]) {/////////////////////////////////////////////
 	pinMode(trigPin, OUTPUT);
 	pinMode(echoPin, INPUT);
 	pinMode(servoPin_US,OUTPUT);
+//LineFollower
+	pinMode(linePinR,INPUT);
+	pinMode(linePinL,INPUT);
+	
 //Main-Loop Section
 	printf("\n All Threads up: RemoteCar starting \n");
 
