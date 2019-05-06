7190715f171bfab78fa9a9571ab6ac68d27a0446
Neuroplant <m.roych@gmail.com>
2019-05-06 15:06:31 +0200

    Update LineFollowCar.c

--- LineFollowCar.c
+++ LineFollowCar.c
@@ -2,14 +2,13 @@
  * Author: Neuroplant
  *
  * Description:
- * Reads joystick(Sixaxis3) events and controls Car.
- * Additional control via LineFollower
+ * control Car via LineFollower + Ultrasonich sensor
  *
  * Compile:
-*  make Remote
+*  make Line
  *
  * Run:
- * sudo ./Remote [m]
+ * sudo ./Line [m]
  */
  
  // Include Section
@@ -114,62 +113,6 @@ int init_Sound (void) {
 		}
 	}
 	return 0;
-}
-
-// Turret/////////////////////////////////////////////////
-// turr1X	-	10..-10	-	x-axis 
-// turr1Y	-	10..-10	-	y-axis
-// turret1	-	0/1	-	laser off/on
-
-int turr1X	= 0, turr1Y 	= -10, turret1 = 	0; 
-void *TurretThread (void *value) {
-	printf("Turret1 ready\n");
-	while (run) {
-		switch (turret1) {
-			case 0 : 	// dont move
-			break;
-			case 1 : // center
-				turr1X	 = 0;
-				turr1Y = -10;
-			break;
-			case 3 :	// Laser on
-				Sound[4].loop = 1;
-				Blinker[0].dura = 10;
-				Blinker[0].freq = 0;
-			break;
-			case 9 :	// laser off
-				Blinker[0].dura = 0;
-				Blinker[0].freq = 0;
-			break;
-			case 8 :	//up
-				turr1Y++;
-				delay(50);
-			break;
-			case 2 :	//down
-				turr1Y--;
-				delay(50);
-			break;
-			case 4 :	//left
-				turr1X--;
-				delay(100);
-			break;
-			case 6 :	//right
-				turr1X++;
-				delay(100);
-			break;
-			default :	// dont move;
-			break;
-		}
-		if (turr1X > 10) turr1X = 10;
-		if (turr1X <-10) turr1X =-10;
-		if (turr1Y > 10) turr1Y = 10;
-		if (turr1Y <-10) turr1Y =-10;
-		
-		servoWriteMS(servoPin_CX,map(turr1X,-10,10,SERVO_MIN_CX,SERVO_MAX_CX));
-		servoWriteMS(servoPin_CY,map(turr1Y,-10,10,SERVO_MIN_CY,SERVO_MAX_CY));
-	}
-	printf("Turret1 off\n");
-	return NULL;
 }
 
 //Blinker////////////////////////////////////////////////
