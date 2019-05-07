576dbf65fe2983bd623bf26e480e3ed2f08372f6
Neuroplant <m.roych@gmail.com>
2019-05-07 20:33:06 +0200

    Update LineFollowCar.c

--- LineFollowCar.c
+++ LineFollowCar.c
@@ -327,64 +327,6 @@ int StickControl(int stick, int value) {
 	return 0;
 }
 
-int read_event(int fd, struct js_event *event) {
-    ssize_t bytes;
-    bytes = read(fd, event, sizeof(*event));
-    if (bytes == sizeof(*event)) return 0;
-    /* Error, could not read full event. */
-    return -1;
-}
-
-size_t get_axis_count(int fd) {
-    __u8 axes;
-    if (ioctl(fd, JSIOCGAXES, &axes) == -1)
-        return 0;
-    return axes;
-}
-
-size_t get_button_count(int fd) {
-    __u8 buttons;
-    if (ioctl(fd, JSIOCGBUTTONS, &buttons) == -1)
-        return 0;
-    return buttons;
-}
-
-void *StickThread (void *value) {
-	printf("StickThread start\n");
-	while ((read_event(js, &event) == 0)&&(run)) {
-		switch (event.type){
-			case JS_EVENT_BUTTON:
-				ButtonControl(event.number,event.value);
-			break;
-			case JS_EVENT_AXIS:
-				StickControl(event.number,event.value);
-			break;
-		}
-	}
-	printf("StickThread end\n");
-	return NULL;
-}
-
-int init_Joystick(void) {
-	js = open(device, O_RDONLY);
-	while (js == -1) {
-	    	js = open(device, O_RDONLY);
-        	Blinker[5].dura = 2;
-		Blinker[5].freq = 20;
-		delay(2000);
-		printf("Warte auf Joystick\n");
-	};
-	printf("Joystick ready \n");
-
-	pthread_t t_Joystick;
-		if(pthread_create(&t_Joystick, NULL, StickThread, NULL)) {
-			printf("Error creating thread t_Joystick\n");
-			return 1;
-		}	
-}
-
-
-
 int ButtonControl (int button, int value) {
 //  PRESS							RELEASE
 /*
@@ -420,7 +362,6 @@ int ButtonControl (int button, int value) {
 				turret1 = 3;
 			break;
 			case 3 :		//quadrat
-				encoder_mode_Switch(-1);
 			break;
 			case 4	:	//L1
 				gear = -1;
@@ -487,6 +428,66 @@ int ButtonControl (int button, int value) {
 	}
 	return 0;
 }
+
+int read_event(int fd, struct js_event *event) {
+    ssize_t bytes;
+    bytes = read(fd, event, sizeof(*event));
+    if (bytes == sizeof(*event)) return 0;
+    /* Error, could not read full event. */
+    return -1;
+}
+
+size_t get_axis_count(int fd) {
+    __u8 axes;
+    if (ioctl(fd, JSIOCGAXES, &axes) == -1)
+        return 0;
+    return axes;
+}
+
+size_t get_button_count(int fd) {
+    __u8 buttons;
+    if (ioctl(fd, JSIOCGBUTTONS, &buttons) == -1)
+        return 0;
+    return buttons;
+}
+
+void *StickThread (void *value) {
+	printf("StickThread start\n");
+	while ((read_event(js, &event) == 0)&&(run)) {
+		switch (event.type){
+			case JS_EVENT_BUTTON:
+				ButtonControl(event.number,event.value);
+			break;
+			case JS_EVENT_AXIS:
+				StickControl(event.number,event.value);
+			break;
+		}
+	}
+	printf("StickThread end\n");
+	return NULL;
+}
+
+int init_Joystick(void) {
+	js = open(device, O_RDONLY);
+	while (js == -1) {
+	    	js = open(device, O_RDONLY);
+        	Blinker[5].dura = 2;
+		Blinker[5].freq = 20;
+		delay(2000);
+		printf("Warte auf Joystick\n");
+	};
+	printf("Joystick ready \n");
+
+	pthread_t t_Joystick;
+		if(pthread_create(&t_Joystick, NULL, StickThread, NULL)) {
+			printf("Error creating thread t_Joystick\n");
+			return 1;
+		}	
+}
+
+
+
+
 
 //Ultrasonic Obstacle avoidance
 struct timespec Time1;
