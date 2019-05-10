8c19d202a1c2372466b701d62b242f36f39c90fa
Neuroplant <m.roych@gmail.com>
2019-05-10 14:04:40 +0200

    Update line.h
    int init_LineDetect(void)
    int LineDetect(void)

--- line.h
+++ line.h
@@ -1,6 +1,14 @@
+/* Line detector
+int init_LineDetect(void)
+int LineDetect(void)
+*/
+
+#include <wiringPi.h>
+
 int init_LineDetect(void) {
 	pinMode(LinePinL, INPUT);
 	pinMode(LinePinR, INPUT);
+	return 0;
 }
 
 int LineDetect (void) {
@@ -9,4 +17,5 @@ int LineDetect (void) {
 		if ((digitalRead(LinePinL)==LOW) && (digitalRead(LinePinR)==HIGH)) val = 1;
 		if ((digitalRead(LinePinL)==HIGH) && (digitalRead(LinePinR)==LOW)) val = 2;
 		if ((digitalRead(LinePinL)==HIGH) && (digitalRead(LinePinR)==HIGH)) val = 3;
+	return val;
 }
