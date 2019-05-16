72dcbf5e9ef5a43037dd6c81f27bfb72496ac4c5
Neuroplant <m.roych@gmail.com>
2019-05-16 19:22:53 +0200

    Update line.h

--- line.h
+++ line.h
@@ -13,9 +13,9 @@ int init_LineDetect(void) {
 
 int LineDetect (void) {
 	int val;
-		if ((digitalRead(LinePinL)==LOW) && (digitalRead(LinePinR)==LOW)) val = 0;
-		if ((digitalRead(LinePinL)==LOW) && (digitalRead(LinePinR)==HIGH)) val = 1;
-		if ((digitalRead(LinePinL)==HIGH) && (digitalRead(LinePinR)==LOW)) val = 2;
-		if ((digitalRead(LinePinL)==HIGH) && (digitalRead(LinePinR)==HIGH)) val = 3;
+		if ((digitalRead(LinePinL)==LOW) && (digitalRead(LinePinR)==LOW)) val = 3;
+		if ((digitalRead(LinePinL)==LOW) && (digitalRead(LinePinR)==HIGH)) val = 2;
+		if ((digitalRead(LinePinL)==HIGH) && (digitalRead(LinePinR)==LOW)) val = 1;
+		if ((digitalRead(LinePinL)==HIGH) && (digitalRead(LinePinR)==HIGH)) val = 0;
 	return val;
 }
