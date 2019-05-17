bf6c6b42f898e39c16165163985a6b1bc460ebdf
Neuroplant <m.roych@gmail.com>
2019-05-17 10:06:44 +0200

    Update sonar.h

--- sonar.h
+++ sonar.h
@@ -6,21 +6,47 @@
 struct timespec Time0, Time1; 
 long StartTime, EndTime, OutTime; 
 
-long PulseLen (int inpin) { 
+
+int pulseIn(int pin, int level, int timeout) {
+   struct timeval tn, t0, t1;
+   long micros;
+   gettimeofday(&t0, NULL);
+   micros = 0;
+   while (digitalRead(pin) != level)
+   {
+      gettimeofday(&tn, NULL);
+      if (tn.tv_sec > t0.tv_sec) micros = 1000000L; else micros = 0;
+      micros += (tn.tv_usec - t0.tv_usec);
+      if (micros > timeout) return 0;
+   }
+   gettimeofday(&t1, NULL);
+   while (digitalRead(pin) == level)
+   {
+      gettimeofday(&tn, NULL);
+      if (tn.tv_sec > t0.tv_sec) micros = 1000000L; else micros = 0;
+      micros = micros + (tn.tv_usec - t0.tv_usec);
+      if (micros > timeout) return 0;
+   }
+   if (tn.tv_sec > t1.tv_sec) micros = 1000000L; else micros = 0;
+   micros = micros + (tn.tv_usec - t1.tv_usec);
+   return micros;
+}
+
+long PulseLen (int inpin, int level, int timeout) { 
 	long val;
 	clock_gettime(CLOCK_REALTIME, &Time0);
 	OutTime = Time0.tv_nsec;
-	while (digitalRead(inpin) == LOW) { 
+	while (digitalRead(inpin) <> level) { 
 		clock_gettime(CLOCK_REALTIME, &Time1); 
 		StartTime  = Time1.tv_nsec; 
-		if (StartTime-OutTime >= 13200) {val = 13201; break;}
+		if (StartTime-OutTime >= timeout) {val = 13201; break;}
 	} 
 	clock_gettime(CLOCK_REALTIME, &Time0);
 	OutTime = Time0.tv_nsec;
-	while (digitalRead(inpin) == HIGH) { 
+	while (digitalRead(inpin) == level) { 
 		clock_gettime(CLOCK_REALTIME, &Time1); 
 		EndTime = Time1.tv_nsec; 
-		if (EndTime-OutTime >= 13200) {val = 13200; break;}
+		if (EndTime-OutTime >= timeout) {val = 13200; break;}
 	} 
 	return (EndTime-StartTime); 
 }
@@ -29,7 +55,10 @@ float getSonar() {
 	digitalWrite(trigPin,HIGH); 
 	delayMicroseconds(10); 
 	digitalWrite(trigPin,LOW); 
-	return ((float)PulseLen(echoPin) * 340.0 / 2.0 / 10000.0); 
+	pingTime = pulseIn(echoPin,HIGH,13200);   //read plus time of echoPin
+	//pingTime = PulseLen(echoPin,HIGH,13200); //have to take a look, why my function does not work
+	distance = (float)pingTime * 340.0 / 2.0 / 10000.0; // the sound speed is 340m/s,and calculate distance
+	return distance;
 }  
 
 float getSonarP(int angle) { 
