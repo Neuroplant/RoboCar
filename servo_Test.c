46aeb7327f64f743b018c0e03882ec8bab9a8eef
Neuroplant <m.roych@gmail.com>
2019-06-17 19:46:48 +0200

    Update servo_Test.c

--- servo_Test.c
+++ servo_Test.c
@@ -13,13 +13,11 @@ void setup() {
 // 	wiringPi
 	if(wiringPiSetup() == -1){ 
         printf("setup wiringPi faiservo !");
-        return 1; 
 	};
 //	wiringPi I2C
 	if(wiringPiI2CSetup(DEV_ID0) == -1){ 
        	printf("setup wiringPi I2C faiservo !");
-       	return 1; 
-	};
+       	}
 // wiringPi PCA9685 (Servo Driver)	
 	int fd = pca9685Setup(PIN_BASE0, DEV_ID0, HERTZ);
 	if (fd < 0) {
@@ -28,16 +26,16 @@ void setup() {
 	}
 	pinMode(DEV_ID0_enable,OUTPUT);
 	digitalWrite(DEV_ID0_enable,LOW);
-	for (int i=0:i<16;i++) servoInit(i+PIN_BASE0);
+	for (int i=0;i<16;i++) servoInit(i+PIN_BASE0);
 }
 
 void loop() {
 	for (int i=0;i<16;i++) {
 		printf(" Servo Nr.%i \n",i);
 		for (int ms= SERVO_MIN_MS; ms <= SERVO_MAX_MS; i++) 
-			servoWriteMS(i+PIN_BASE0, int ms);		
+			servoWriteMS(i+PIN_BASE0, ms);		
 		for (int ms= SERVO_MAX_MS; ms <= SERVO_MIN_MS; i--) 
-			servoWriteMS(i+PIN_BASE0, int ms);
+			servoWriteMS(i+PIN_BASE0, ms);
 	}
 	printf("\n");
 }
