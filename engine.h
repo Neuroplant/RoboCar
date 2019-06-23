e92bbd3826be8251d2e0aad3d33e04e9c88aabc7
Neuroplant <m.roych@gmail.com>
2019-06-23 18:42:10 +0200

    Update engine.h

--- engine.h
+++ engine.h
@@ -39,13 +39,13 @@ void *EngineThread(void *value){
 			pwmWrite(motorPin1,abs(throttle));
 			pwmWrite(motorPin2,0);
 		//printf("turn Back...\n");
-			Sound[2].loop = 1;
+			//Sound[2].loop = 1;
 		}
 		if (gear == 0 ){
 			pwmWrite(motorPin1,0);
 			pwmWrite(motorPin2,0);
-			Blinker[6].dura = 1;
-			Blinker[6].freq = 0;
+			//Blinker[6].dura = 1;
+			//Blinker[6].freq = 0;
 		}
 		servoWriteMS(servoPin_ST,map(steering,10,-10,SERVO_MIN_ST,SERVO_MAX_ST));
 	}
